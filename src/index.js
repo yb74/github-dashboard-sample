78bd7ffb20f817de249f6c2621a6e0c14c4ca513
yb74 <54204783+yb74@users.noreply.github.com>
2021-10-02 10:53:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+        <App />
+    </Router>,
   document.getElementById('root')
 );
 
